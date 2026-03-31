5a2914bdd83df69d991f812cfa30b3940c8f0e71
PedroSaturno <pedro1saturno@gmail.com>
2026-03-30 21:56:48 -0300

    criação do profile.ts

---
+++ src/data/profile.ts
@@ -0,0 +1,28 @@
+export const profileData = {
+  name: 'satboy',
+  avatar: '/glasses.jpg',
+  tagline: 'tech enthusiast & fps player',
+  bio: 'god <span class="text-white drop-shadow-[0_0_5px_white]">did</span>.',
+  socialLinks: [
+    {
+      name: 'discord',
+      icon: 'discord',
+      url: 'https://discord.com/users/977624630187659264',
+    },
+    {
+      name: 'github',
+      icon: 'github',
+      url: 'https://github.com/eupedrx',
+    },
+    {
+      name: 'steam',
+      icon: 'steam',
+      url: 'https://steamcommunity.com/id/theylovesaturn/',
+    },
+    {
+      name: 'spotify',
+      icon: 'spotify',
+      url: 'https://open.spotify.com/user/n0tf3twqx8xbjjzgnmntm89qu?si=667f173ba4634d82',
+    },
+  ],
+};
