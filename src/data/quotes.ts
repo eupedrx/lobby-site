7b1b19b28bf166a4cefcd3e7b699fa20aa24ccdd
PedroSaturno <pedro1saturno@gmail.com>
2026-04-09 01:02:19 -0300

    feat criação da seção de citações e também o modo de adiconar uma

---
+++ src/data/quotes.ts
@@ -0,0 +1,10 @@
+export const quotes = [
+  {
+    author: 'Pharell Williams',
+    quote: 'Stay loyal to your creativity because it’s a gift.',
+  },
+  {
+    author: 'A native of Bangkok',
+    quote: 'Sometimes you mix the past with the present, and your happiness disappears.',
+  }
+];
